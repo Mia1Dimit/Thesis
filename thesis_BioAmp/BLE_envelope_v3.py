97ecd6165300c31f1983210fb5e88cd56e08ea56
Mia1Dimit <151967153+Mia1Dimit@users.noreply.github.com>
2024-01-23 19:52:54 +0200

    Add files via upload

--- thesis_BioAmp/BLE_envelope_v3.py
+++ thesis_BioAmp/BLE_envelope_v3.py
@@ -8,23 +8,26 @@ device_address = 'f4:12:fa:63:47:29'
 filtered_characteristic_uuid = "a3b1a544-8794-11ee-b9d1-0242ac120002"
 
 class RealTimePlotter:
-    def __init__(self, window_size=1500):
+    def __init__(self, window_size=1000):
         self.received_data = bytearray()
         self.amplitudes = []
         self.envelope_values = []  
         self.rms_values = []
         self.iemg_values = []
-        #self.frequency_values = []
+        self.fft_values = []
         self.mnf_values = []
         self.mpf_values = []
         self.sample_count = 0
         self.window_size = window_size
+        self.start_index = 0
+        self.end_index = 1000
 
         # First figure for EMG signal and its envelope
         plt.ion()
-        self.fig_emg, ( self.ax_emg, self.ax_env, self.ax_rms, self.ax_iemg, self.ax_mnf) = plt.subplots(5)
+        self.fig_emg, ( self.ax_emg, self.ax_fft, self.ax_rms, self.ax_iemg, self.ax_mnf) = plt.subplots(5)
         self.line_emg, = self.ax_emg.plot([], [], 'b-', label='EMG Data')
-        self.envelope_line_emg, = self.ax_env.plot([], [], 'r-', label='Envelope')
+        self.envelope_line_emg, = self.ax_emg.plot([], [], 'r-', label='Envelope')
+        self.line_fft, = self.ax_fft.plot([], [], 'b-', label='FFT')
         self.line_rms, = self.ax_rms.plot([], [], 'g-', label='RMS')
         self.line_iemg, = self.ax_iemg.plot([], [], 'm-', label='IEMG')
         self.line_mnf, = self.ax_mnf.plot([], [], 'r-', label='MNF')
@@ -35,20 +38,20 @@ class RealTimePlotter:
         self.ax_emg.set_ylabel('EMG Amplitude')
         #self.ax_emg.set_title('Real-time EMG Data')
         self.ax_emg.set_ylim(-75.0, 75.0)
-        self.ax_emg.set_xlim(0, self.window_size)
+        #self.ax_emg.set_xlim(0, self.window_size)
         self.ax_emg.legend()
 
-        self.ax_env.set_xlabel('Number of Samples')
-        self.ax_env.set_ylabel('EMG Envelope')
-        #self.ax_env.set_title('Envelope EMG')
-        self.ax_env.set_ylim(-75.0, 75.0)
-        self.ax_env.set_xlim(0, self.window_size)
-        self.ax_env.legend()
+        self.ax_fft.set_xlabel('Frequency (Hz)')
+        self.ax_fft.set_ylabel('Power')
+        #self.ax_fft.set_title('FFT EMG')
+        self.ax_fft.set_ylim(-2000.0, 2000.0)
+        #self.ax_fft.set_xlim(0, self.window_size)
+        self.ax_fft.legend()
 
         self.ax_rms.set_xlabel('Number of Samples')
         self.ax_rms.set_ylabel('RMS Value')
         #self.ax_rms.set_title('RMS')
-        self.ax_rms.set_ylim(-60.0, 60.0)
+        self.ax_rms.set_ylim(-100.0, 100.0)
         self.ax_rms.legend()
 
         self.ax_iemg.set_xlabel('Number of Samples')
@@ -61,7 +64,7 @@ class RealTimePlotter:
         self.ax_mnf.set_xlabel('Number of Samples')
         self.ax_mnf.set_ylabel('Frequency (Hz)')
         #self.ax_mnf.set_title('MNF/MPF')
-        self.ax_mnf.set_ylim(-250.0, 250.0)  
+        self.ax_mnf.set_ylim(-500.0, 750.0)  
         self.ax_mnf.legend()
 
         self.buffer_size = 208
@@ -71,33 +74,41 @@ class RealTimePlotter:
 
     # Modify the set_data_and_plot method
     def set_data_and_plot(self):
-        #print("Length of mnf_values:", len(self.mnf_values))
-        #print("Length of mpf_values:", len(self.mpf_values))
-
         #start_index = max(0, self.sample_count - self.window_size)
         #end_index = min(self.sample_count, start_index + self.window_size)
 
         # Update EMG signal and envelope plot
         self.line_emg.set_data(range(0, len(self.amplitudes)), self.amplitudes[0:len(self.amplitudes)])        
-        self.ax_emg.set_xlim(0, self.sample_count)
         self.envelope_line_emg.set_data(range(0, len(self.envelope_values)), self.envelope_values[0:len(self.envelope_values)])
-        self.ax_env.set_xlim(0, self.sample_count)
+        self.ax_emg.set_xlim(0, self.sample_count)
+        
 
-        # Update RMS, IEMG, and MeanAbsVal plots
+        if(len(self.fft_values)>=1000):
+            fft_freq = np.fft.fftfreq(1000, d=1/500)
+            print("frequencies: ", len(fft_freq))
+            print("magnitudes: ", self.fft_values[-1000:])
+            self.line_fft.set_data(fft_freq , self.fft_values[-1000:])
+            self.ax_fft.set_xlim(0, len(fft_freq)/2)
+
+        # Update RMS, IEMG plots
         self.line_rms.set_data(range(0, len(self.rms_values)), self.rms_values[0:len(self.rms_values)])
-        self.ax_rms.set_xlim(0, self.sample_count)
-        self.line_iemg.set_data(range(0, len(self.iemg_values)), self.iemg_values[0:len(self.iemg_values)])
-        self.ax_iemg.set_xlim(0, self.sample_count)
+        self.ax_rms.set_xlim(0, len(self.rms_values))
 
+        self.line_iemg.set_data(range(0, len(self.iemg_values)), self.iemg_values[0:len(self.iemg_values)])
+        self.ax_iemg.set_xlim(0, len(self.iemg_values))
+
+        print("Length of mnf_values:", self.mnf_values[0:len(self.mnf_values)])
+        print("Length of mpf_values:", self.mpf_values[0:len(self.mpf_values)])
         self.line_mnf.set_data(range(0, len(self.mnf_values)), self.mnf_values[0:len(self.mnf_values)])
         self.line_mpf.set_data(range(0, len(self.mpf_values)), self.mpf_values[0:len(self.mpf_values)])
-        self.ax_mnf.set_xlim(0, self.sample_count)
+        self.ax_mnf.set_xlim(0, len(self.mnf_values))
 
         plt.pause(0.01)
 
 
     def update_received_data(self, data):
         self.received_data.extend(data)
+        
         while len(self.received_data) >= 208:
             packet = self.received_data[:208]
             self.received_data = self.received_data[208:]
@@ -108,18 +119,23 @@ class RealTimePlotter:
             for val in floats:
                 self.envelope_values.append(self.calculate_envelope(abs(val)))
 
-            step = 100
-            for i in range(0, len(self.amplitudes) - 1000 + 1, step):
-                epoch_data = self.amplitudes[i:i+1000]
-                self.rms_values.append(self.calculate_rms(epoch_data))
-                self.iemg_values.append(self.calculate_iemg(epoch_data))
+            print("Length of amplitudes:", len(self.amplitudes))
+            print("Length of envelope_values:", len(self.envelope_values))
 
-                fft_result = np.fft.fft(epoch_data)
-                #magnitude_values = np.abs(fft_result)
-                freq_values = np.fft.fftfreq(len(epoch_data), d=1/500) 
-                #self.frequency_values.extend(fft_result)
-                self.mnf_values.append(self.calculate_mnf(freq_values, fft_result))
-                self.mpf_values.append(self.calculate_mpf(freq_values, fft_result))    
+        step = 100
+        if (len(self.amplitudes) >= 1000 + self.start_index):                
+            epoch_data = self.amplitudes[self.start_index:self.end_index]
+            self.rms_values.append(self.calculate_rms(epoch_data))
+            self.iemg_values.append(self.calculate_iemg(epoch_data))
+
+            fft_result = np.fft.fft(epoch_data)
+            freq_values = np.fft.fftfreq(len(epoch_data), d=1/500) 
+            self.fft_values.extend(np.abs(fft_result))
+            self.mnf_values.append(self.calculate_mnf(freq_values, fft_result))
+            self.mpf_values.append(self.calculate_mpf(freq_values, fft_result))
+            self.start_index +=step
+            self.end_index = self.window_size + self.start_index
+        
 
     def calculate_envelope(self, abs_emg_value):
         self.sum_buffer -= self.circular_buffer[self.data_index]
@@ -135,7 +151,7 @@ class RealTimePlotter:
         return np.sum(np.abs(iemg_values)) 
     
     def calculate_mnf(self, freq_values, fft_result):
-        mnf = np.sum(freq_values * np.abs(fft_result)) / np.sum(np.abs(fft_result))
+        mnf = np.sum(freq_values * np.abs(fft_result)**2) / np.sum(np.abs(fft_result)**2)
         return mnf
 
     def calculate_mpf(self, freq_values, fft_result):
